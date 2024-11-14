c4d316e54fd15234f6ab1f62ef416dde5b95b7d2
kas <kas@ableton.com>
2024-11-14 11:59:12 +0100

    Introduce normals for side collisions

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
+#include "Vector.hpp"
 
 namespace bd {
+
+constexpr inline Vector kLeftSideNormal{1.0, 0.0};
+constexpr inline Vector kRightSideNormal{-1.0, 0.0};
+constexpr inline Vector kBottomSideNormal{0.0, 1.0};
+constexpr inline Vector kOverSideNormal{0.0, -1.0};
+
 constexpr unsigned int kWindowSizeX = 880;
 constexpr float kWindowSizeY = 880.f;
 
